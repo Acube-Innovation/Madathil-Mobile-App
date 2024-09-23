0a82b90f31e3492dff482585b5ab3cb136d7f3bb
soneevacube <soneev@acube.co>
2024-09-23 12:00:02 +0530

    first-commit

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: providerList(),
       child: MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Madathil',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
